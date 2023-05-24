0cc557730336b460289f05d168d13774aea83d49
composercordero <composercordero@gmail.com>
2023-05-23 22:01:08 -0500

    Create Captcha and user-validation
    Created a simple number generator and user-validation. It does not
    prevent the form to be submitted yet!

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 const feelings = document.querySelector('.feelings');
+const captcha = document.querySelector('.captcha');
+const captchaInput = document.querySelector('.captcha-input');
+const share = document.querySelector('.share');
+
+share.addEventListener('click', function () {
+    const checkCaptcha = captchaInput.value === captchaInput.placeholder ? 
+    captchaInput.classList.add('valid') : captchaInput.classList.add('invalid');
+});
+
+getRndInteger(100,999);
+
+
+//creating the options for the drop down menu
 
 for (let i = 0; i <= 9; i++){
     const option = document.createElement('option');
     const choice = i+1 < 10? i+1 : 10;
-    option.value = choice;
-    option.text = choice;
+    if (i + 1 === 5){
+        option.selected = "selected";}
+    if (i + 1 || i + 1 === 5){
+        option.value = choice;
+        option.text = choice;
+    }
     feelings.appendChild(option);
 }
+
+//generate random number for captcha
+
+function getRndInteger(min, max) {
+    const randomNumber = Math.floor(Math.random() * (max - min)) + min;
+    captcha.textContent += randomNumber;
+    captchaInput.placeholder = randomNumber;
+  }
+
+
+
+
+// letter.classList.add("valid")
